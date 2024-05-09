8cbf9faab3925f256f8c7ac1d4125535383f88d6
Hoàng Trung <trung@MacBook-Pros-Trung.local>
2024-05-09 17:15:00 +0700

    use swangger

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { json, urlencoded } from 'express';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   dotenv.config();
@@ -21,7 +21,17 @@ async function bootstrap() {
   const port = process.env.APP_PORT || 3000;
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS API')
+    .setDescription('API Documentation')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(port);
-  console.log(`Server is running on port: ${port} `)
+  console.log(`Server is running on port: ${port} `);
 }
+
 bootstrap();
